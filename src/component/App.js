24c40daafc5b6269a9e34f63fa5b8505b7231013
Arsenii Machekhin <arsenium2@gmail.com>
2022-11-02 23:17:14 +0300

    пр15 1.2.7

--- src/component/App.js
+++ src/component/App.js
@@ -149,6 +149,7 @@ function App() {
     // Отправляем запрос в API и получаем обновлённые данные карточки
     api.toggleLike(card._id, isLiked)
       .then((newCard) => {
+        console.log(newCard);
         setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
       })
       .catch((err) => {
